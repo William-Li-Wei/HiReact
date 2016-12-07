bd8849cc503a4b1d8cbf9305045046be5dfa7421
Wei Li <wei.li@catenate.com>
2016-12-07 13:52:50 +0100

    demo to JSX characteristics.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 
 class App extends React.Component {
-   render() {
-      return (
-         <div>
-            Hello World!
-         </div>
-      );
-   }
+   	render() {
+   		var i = 1;
+   		var myStyle = {
+   			fontSize: 20,
+   			color: 'red'
+   		};
+
+		return (
+         	<div>
+            	<h1>Header</h1>
+            	<h3>Subtitle</h3>
+            	<p data-attribute="somevalue" style={myStyle}>
+            		{/* this line is colored*/}
+            		This is sooooo { i == 1 ? 'NICE!' : 'BAD!' }            		
+            	</p>
+         	</div>
+      	);
+   	}
 }
 
 export default App;
